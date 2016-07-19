0bf6333dc1ed5a23cebe60e853fdd2ed5d9415da
Hendrik Eeckhaut <hendrik.eeckhaut@sigasi.com>
2016-07-19 22:20:50 +0200

    Improved foto's page

--- _python/hooks.py
+++ _python/hooks.py
@@ -2,13 +2,15 @@ def process_info(info, site):
     if 'layout' in info:
         if info['layout']=='gallery2':
             process_gallery(info)
-
 from os import getcwd
 from os import listdir
 from os.path import isfile, join
+
 def process_gallery(info):
     if 'folder' in info:
-       mypath=getcwd()+"/images/fotos/"+info['folder']+"/"
-       onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-       #print(onlyfiles)
-       info['fotos'] = onlyfiles
+        mypath=getcwd()+"/images/fotos/"+info['folder']+"/"
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        #print(onlyfiles)
+        info['fotos'] = onlyfiles
+        if not 'thumbnail' in info:
+            info['thumbnail'] = onlyfiles[0]
