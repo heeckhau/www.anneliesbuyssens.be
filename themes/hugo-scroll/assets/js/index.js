640b2b922d3d62f6c97c98eb1aacd6f3254c44cc
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2026-09-24 16:45:54 +0200

    Faster page loads: drop icon font and unused scripts
    - Replace the Fork Awesome icon font (108 KB) with inline SVG icons
      for the three icons in use (partials/icon.html)
    - List bullets are plain CSS instead of icons inserted by JavaScript
    - Remove icons.js (unused @fa-name@ replacement) and the IE11
      css-vars ponyfill
    
    Homepage transfer size goes from 258 KB to about 128 KB.

--- themes/hugo-scroll/assets/js/index.js
+++ themes/hugo-scroll/assets/js/index.js
@@ -87,32 +87,5 @@ var $sitehead = $("#site-head");
         });
       });
     }
-
-    $('ul').addClass("fa-ul");
-    $("ul li").prepend('<span class="fa-li"><i class="fa fa-circle-thin"></i></span>');
-    $("blockquote p").prepend('<span class="quo fa fa-quote-left"></span>');
-    $("blockquote p").append('<span class="quo fa fa-quote-right"></span>');
-  });
-
-  $post.each(function () {
-    var postText = $(this).html();
-    var fa = [];
-    for (var i = 0; i < icons.length; i++) {
-      fa[i] = {};
-      fa[i].str = "@" + icons[i] + "@";
-      fa[i].icon = icons[i];
-      fa[i].int = postText.search(fa[i].str);
-
-      if (fa[i].int > -1) {
-        fa[i].count = postText.match(new RegExp(fa[i].str, "g")).length;
-        for (var j = 0; j < fa[i].count; j++) {
-          $(this).html(
-            $(this)
-              .html()
-              .replace(fa[i].str, "<i class='fa " + fa[i].icon + "'></i>")
-          );
-        }
-      }
-    }
   });
 })(jQuery);
